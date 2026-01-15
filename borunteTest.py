bffcac18b7899a96d76d167a31f2947c3fb111ac
zwb800 <zwb800@gmail.com>
2026-01-15 12:59:56 +0800

    test position point

--- borunteTest.py
+++ borunteTest.py
@@ -1,14 +1,14 @@
 
 import borunte
 
-# IP = "192.168.3.1"
-IP = "127.0.0.1"
+IP = "192.168.3.1"
+# IP = "127.0.0.1"
 PORT = 9760
 
 robot = borunte.Borunte(IP,PORT)
 robot.open()
 # robot.version()
-# robot.getAxisPosition(0)
-robot.position(16,114,849,39,3.9,-9.7)
-robot.setIO(4,6,1)
+#robot.getAxisPosition(1)
+robot.position(0,200,900,40,0,0)
+#robot.setIO(4,6,1)
 robot.close()
